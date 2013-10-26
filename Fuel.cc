616bdd127e958092893fe5fc940053a12b550a37
Darragh Downey <darragh.downey90@gmail.com>
2013-10-26 23:01:11 +0100

    initial commit

---
+++ Fuel.cc
@@ -0,0 +1,5 @@
+class Fuel
+{
+	public:
+	private:
+}
---
+++ PowerPlant.cc
@@ -0,0 +1,8 @@
+class PowerPlant
+{
+	public:
+		generate(Fuel fuel);
+	private:
+		int engineers; //number of engineers needed to operate
+		int builders; //number of builders needed to build		
+}
